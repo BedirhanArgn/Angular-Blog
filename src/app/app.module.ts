b254d9cb80a358d11e2df816b72794e1dc4956eb
BedirhanArgn <argunbedir@gmail.com>
2020-07-29 23:05:50 +0300

    post component and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireAuthModule } from "@angular/fire/auth";
+import { PostsModule } from './posts/posts.module';
+import{RouterModule,Routes} from '@angular/router';
+const routes:Routes= [
+{path:'',redirectTo:'/blog',pathMatch:'full'},
+{path:'',loadChildren:'./posts/post.module#PostsModulew'}
+
+]
+
 
 @NgModule({
   declarations: [
@@ -22,7 +30,9 @@ import { AngularFireAuthModule } from "@angular/fire/auth";
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PostsModule,
+    RouterModule.forChild(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
