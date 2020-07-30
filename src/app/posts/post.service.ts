169663bf7d3b078f8d62400a5d4169ae795ea5b2
BedirhanArgn <argunbedir@gmail.com>
2020-07-30 21:53:34 +0300

    firebase connection

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import {Post} from './post'
 @Injectable({
   providedIn: 'root'
 })
+//Hold data form post 
 export class PostService {
+  postCollection:AngularFirestoreCollection<Post>
 
-  constructor() { }
+  constructor(private afs:AngularFirestore) {
+    this.postCollection=this.afs.collection('posts',ref=> //burası firestore'a bağlantı kuruyor.posts ordada belirttiğimiz isim 
+    ref.orderBy('published','desc')) 
+  }
 }
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -10,7 +10,6 @@ const routes:Routes=[
 {path:'blog',component:PostListComponent},
 {path:'blog/:id',component:PostDetailComponent},
 {path:'dashboard',component:PostDashboardComponent}
-
 ]
 
 @NgModule({
