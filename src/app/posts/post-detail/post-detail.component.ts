2d97e1bf45a2f1e6bd51242e482f2974b958f7ce
BedirhanArgn <argunbedir@gmail.com>
2020-08-04 03:12:56 +0300

    get all users and get by id

--- src/app/posts/post-detail/post-detail.component.ts
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Post} from '../post';
+import {ActivatedRoute} from '@angular/router'; //Route'ın yakalanması için bu sınıfa ihtiyacımız var.
+import{PostService} from '../post.service';
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.css']
 })
 export class PostDetailComponent implements OnInit {
-
-  constructor() { }
+  post:Post
+  constructor(private route:ActivatedRoute,private postService:PostService) { }
 
   ngOnInit(): void {
+    this.getPost()
+  } 
+
+  getPost() {
+    const id =this.route.snapshot.paramMap.get('id')
+    return this.postService.getPostData(id).subscribe(data=>this.post=data)
+
   }
+
+
+
 
 }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,16 +1,63 @@
 import { Injectable } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import {Post} from './post'
+import { AngularFirestore, AngularFirestoreCollection,AngularFirestoreDocument } from '@angular/fire/firestore';
+import {Post} from './post';
+import {map}  from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 //Hold data form post 
 export class PostService {
   postCollection:AngularFirestoreCollection<Post>
-
+  postDoc:AngularFirestoreDocument<Post> //id'si olanın detailini getirmek için kullanılıyor.
   constructor(private afs:AngularFirestore) {
     this.postCollection=this.afs.collection('posts',ref=> //burası firestore'a bağlantı kuruyor.posts ordada belirttiğimiz isim 
     ref.orderBy('published','desc')) 
+  }
+
+
+  getPosts() {
+    return this.postCollection.snapshotChanges().pipe(map(actions=>{
+      return actions.map(a=>{
+        const data=a.payload.doc.data() as Post 
+        const id=a.payload.doc.id
+        return {id,...data}
+      })
+    }))
+  }
+
+  getPostData(id:string){
+//
+//this.postDoc=this.afs.doc<Post>('posts/${id}')
+//return this.postDoc.valueChanges()
+   /*
+    return this.afs.collection('blogapp').doc(id).ref.get()
+     .then((doc) => {
+      console.log(doc.data())
+       return doc.data();   // here...
+     });*/
+    // return this.afs.collection('blogapp').doc<Post>(id).valueChanges()
+
+this.postDoc=this.afs.collection<Post>('posts').doc<Post>(id)
+return this.postDoc.valueChanges()
+
+/*
+    return this.afs
+    .collection<Post>('posts')
+    .doc(id)
+    .ref
+    .get()
+    .then((doc) => {
+        if (doc.exists) {
+            console.log(doc.data());
+            return doc.data();
+        } else {
+            return console.log('Doc does not exits');
+        }
+     })
+     .catch((err) => {
+        console.error(err);
+     });
+  }
+*/
   }
 }
